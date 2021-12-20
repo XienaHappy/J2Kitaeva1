46ceb3985d7aacabd98b725f8dea8b2029db5886
Xiena Happy <longandhappylife@mail.ru>
2021-12-20 21:48:53 +0300

    GeekBrainsQAJavaCoreHomeWorkOne_XienaHappy

---
+++ src/main/java/LessonOneKitaeva/Cat.java
@@ -0,0 +1,7 @@
+package LessonOneKitaeva;
+
+    public class Cat extends Creature {
+        public Cat(String name) {
+            super("Lion", "Leo", 1000, 0);
+        }
+    }
--- src/main/java/LessonOneKitaeva/Main.java
+++ src/main/java/LessonOneKitaeva/Main.java
@@ -3,37 +3,14 @@ package LessonOneKitaeva;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Dreamteam:");
+        System.out.println("Dreamteam starts the Campionship!");
 
-        Teammate teammate1 = new Teammate("Leopold",110);
+                Championship c = new Championship(new Barrier[]{new Run(200), new SwimingPool(500)});
+                Team team = new Team("Dreamteam", new Competitor[]{new Teammate("Fire bird"), new Cat ("Teammate2"), new Bird("Teammate3")});
+                c.go(team);
+                team.showResults();
 
-        System.out.println(teammate1);
+        System.out.println("Championship completed. Welcome again!");
+            }
 
-        Teammate teammate2 = new Teammate("Matilda", 100);
-
-        System.out.println(teammate2);
- 
-        Teammate teammate3 = new Teammate("Katrin",90);
-
-        System.out.println(teammate3);
-
-        Teammate teammate4 = new Teammate("Samuel", 60);
-
-        System.out.println(teammate4);
-
-
-        System.out.println("To overcome the obstacle need energy:");
-
-        Obstacle Obstacle1 = new Obstacle("Sweeming pool", 60);
-
-        System.out.println(Obstacle1);
-
-        Obstacle Obstacle2 = new Obstacle("Jumping", 20);
-
-        System.out.println(Obstacle2);
-
-        Obstacle Obstacle3 = new Obstacle("Jugging", 20);
-
-        System.out.println(Obstacle3);
         }
-    }
